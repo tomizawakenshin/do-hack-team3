07bea91cd8f9a03a65e813b629ad199908d18881
tomizawakenshin <kenshin.8799@gmail.com>
2024-05-11 22:41:38 +0900

    警告音と色変追加

--- dohack-team3/app/components/Meteor.tsx
+++ dohack-team3/app/components/Meteor.tsx
@@ -1,32 +1,72 @@
 "use client";
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Task } from '../types';
 import styled from 'styled-components'
+import styles from './Meteor.module.css'
+//import Sound from './sound/keihou.mp3';
+//import useSound from 'use-sound';
 
 interface TodoProps {
     todo: Task;
 }
 
 const Meteor = ({todo} : TodoProps) => {
+    const [InDanger, SetInDanger] = useState(true);
     const [year, month, day] = todo.date.split('/');
     const date = new Date();
     //const num = 470 - (Number(month) * 30 + Number(day) - (date.getMonth() + 1) * 30 - date.getDate()) * 1;
-    const num = 400 - (Number(day) - date.getDate()) * 10;
+    const restDay = Number(day) - date.getDate();
+    const num = 400 - (restDay) * 10;
 
-    const MeteorCss = styled.div`
+    const MeteorLocation = styled.div`
     padding-top: ${num}px;
     `;
+    function Play() {
+        if(InDanger) {
+            console.log("再生中");
+            const audio = new Audio('/Audio/keihou.mp3');
+            audio.play();
+            //SetInDanger(false);
+        }
+    }
+
+    useEffect(() => {
+        if(restDay <= 2) {
+            Play();
+            SetInDanger(false);
+        }
+    })
 
   return (
-    <MeteorCss>
-        <div className='border bg-slate-500 rounded-lg'>
-            <div className='m-10 text-white'>
-                {/* {todo.text} */}
-                {num}
+    <MeteorLocation>
+        <div className=' rounded-lg space-y-2'>
+            {/* <div className='m-10 text-white'> */}
+            
+                {restDay > 2 ? (
+                    <div className='border border-black rounded-lg'>
+                        <div className='m-3 space-y-5'>
+                            <div className='items-center'>{todo.text}</div>
+                            <div className='border border-white rounded-lg'>
+                                <div className='m-3'>あと{restDay}日で人類滅亡！</div>
+                            </div>
+                        </div>
+                    </div>
+                 ) : (
+                    <div className='border border-red-500 bg-red-500 rounded-lg'>
+                        <div className='m-3 space-y-5'>
+                            <div className='text-black-500 '>{todo.text}</div>
+                            <div className='border border-white rounded-lg'>
+                                <div className='text-white m-3'>あと{restDay}日で人類滅亡！</div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            <div className={styles.Meteor}>
+                <img src='/image/FireColorMeteor.png' className="" width={100} height={100}/>
             </div>
         </div>
-    </MeteorCss>
+    </MeteorLocation>
   )
 }
 
